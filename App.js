aa8df06db0f9494088ae9409c9b142174163b843
William de Lima Schneider <william.lima.schneider@gmail.com>
2021-01-03 16:37:34 -0300

    animacao sequencia + paralela

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Text, Animated} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Animated } from 'react-native';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
       larAnimada: new Animated.Value(150),
       altAnimada: new Animated.Value(50),
+      opacidadeAnimada: new Animated.Value(0),
     };
 
-    Animated.timing(
-      this.state.altAnimada,
-      {
-        toValue: 150,
-        duration: 2000
-      }
-    ).start();
+    Animated.sequence([
+      Animated.timing(this.state.opacidadeAnimada, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: false,
+      }),
+
+      Animated.parallel([
+        Animated.timing(this.state.larAnimada, {
+          toValue: 300,
+          duration: 500,
+          useNativeDriver: false,
+        }),
+
+        Animated.timing(this.state.altAnimada, {
+          toValue: 200,
+          duration: 500,
+          useNativeDriver: false,
+        }),
+      ]),
+
+      Animated.timing(this.state.opacidadeAnimada, {
+        toValue: 0,
+        duration: 2000,
+        useNativeDriver: false,
+      }),
+    ]).start();
+
+    // Animated.timing(
+    //   this.state.altAnimada,
+    //   {
+    //     toValue: 150,
+    //     duration: 2000,
+    //     useNativeDriver: false
+    //   }
+    // ).start();
   }
 
   render() {
@@ -29,8 +58,9 @@ class App extends Component {
             height: this.state.altAnimada,
             backgroundColor: '#4169E1',
             justifyContent: 'center',
+            opacity: this.state.opacidadeAnimada,
           }}>
-          <Text style={{color: '#FFFFFF', fontSize: 22, textAlign: 'center'}}>
+          <Text style={{ color: '#FFFFFF', fontSize: 22, textAlign: 'center' }}>
             Carregando...
           </Text>
         </Animated.View>
