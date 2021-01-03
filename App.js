3d98312cf261297b9cb040945210ce3b0fa81f27
William de Lima Schneider <william.lima.schneider@gmail.com>
2021-01-03 18:10:26 -0300

    animacao interpolada

--- App.js
+++ App.js
@@ -6,69 +6,33 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      larAnimada: new Animated.Value(150),
-      altAnimada: new Animated.Value(35),
-      opAnimada: new Animated.Value(0)
+      larAnimada: new Animated.Value(0)
     };
-    this.carregarGrafico = this.carregarGrafico.bind(this);
-  }
 
-  carregarGrafico() {
-    Animated.sequence([
-
-      Animated.timing(this.state.opAnimada,{
-        toValue:1,
-        duration: 400,
+    Animated.timing(
+      this.state.larAnimada,
+      {
+        toValue: 100,
+        duration: 9000,
         useNativeDriver: false
-      }),
-      Animated.timing(
-        this.state.altAnimada,
-        { 
-          toValue:300,
-          duration:1000,
-          useNativeDriver: false
-        }
-      ),
-    ]).start();
+      }
+    ).start();
   }
 
   render() {
+    let porcentagemAnimate = this.state.larAnimada.interpolate({
+      inputRange: [0,100],
+      outputRange: ['0%', '100%']
+    });
     return (
-      <View style={{flex: 1}}>
-        <View
+      <View style={styles.container}>
+        <Animated.View
           style={{
-            height: 70,
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'row',
             backgroundColor: '#4169E1',
+            width: porcentagemAnimate,
+            height: 25,
           }}>
-          <TouchableOpacity onPress={this.carregarGrafico}>
-            <Text style={{fontSize: 25, color: 'white'}}>Gerar Grafico</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-          }}>
-            <Text style={{fontSize: 22, textAlign: 'center'}}>Vendas</Text>
-          <Animated.View
-            style={{
-              width: this.state.larAnimada,
-              height: this.state.altAnimada,
-              backgroundColor: '#FF0000',
-              justifyContent: 'center',
-              opacity: this.state.opAnimada,
-            }}>
-            <Text style={{color: '#FFFFFF', fontSize: 22, textAlign: 'center'}}>
-              R$150,00
-            </Text>
           </Animated.View>
-        </View>
       </View>
     );
   }
@@ -78,6 +42,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
   },
 });
