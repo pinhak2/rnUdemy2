0e2fe446bf2c0c963a13331a4840dfd67dafb596
William de Lima Schneider <william.lima.schneider@gmail.com>
2021-01-03 13:03:04 -0300

    tamanhos fixos e dinamicos

--- App.js
+++ App.js
@@ -1,40 +1,19 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
-import Jobs from './src/images/jobs';
-import Skeedoo from './src/images/skeedo';
-import Styles from './src/styles/style';
+import {View, Text} from 'react-native';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Matheus',
-    };
-    this.enter = this.enter.bind(this);
-  }
-
-  enter(name){
-    this.setState({
-      name: name,
-    });
-  }
-
   render() {
-    let nome = 'Matheus';
-
     return (
-      <View style={Styles.a}>
-
-        <Button title="Entrar" color='#FF0FF0' onPress={() => this.enter('João Silva') }/>    
-        <Button title="Entrar" onPress={() => this.enter('Jeremias') }/>    
-        <Text style={Styles.mainText}>
-          Teste {this.state.name}
-        </Text>
-        <Text  style={[Styles.area, Styles.mainText]}>
-          Teste 2 Styles
-        </Text>
-        <Jobs width={500} height={200} name={'Stevão Jobs'}/>
-        <Skeedoo />
+      <View style={{flex: 1, backgroundColor: '#22A'}}>
+        <View style={{flex: 3, backgroundColor: 'red'}}>
+          <Text>Texto aqui!</Text>
+        </View>
+        <View style={{flex: 10, backgroundColor: 'green'}}>
+          <Text>Texto aqui!</Text>
+        </View>
+        <View style={{flex: 4, backgroundColor: 'yellow'}}>
+          <Text>Texto aqui!</Text>
+        </View>
       </View>
     );
   }
